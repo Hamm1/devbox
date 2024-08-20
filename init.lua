dcc32d0d9f3f59436796c30abbaafcf2c3271934
Hamm1 <39998558+Hamm1@users.noreply.github.com>
2024-08-19 20:11:48 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -580,7 +580,7 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
         tsserver = {},
-        tailwindscss = {},
+        tailwindcss = {},
         zls = {},
         astro = {},
         denols = {},
