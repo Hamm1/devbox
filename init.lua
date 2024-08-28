46722d0332007a884e68b85746844899a192d575
Hamm1 <39998558+Hamm1@users.noreply.github.com>
2024-08-28 09:34:51 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -942,15 +942,15 @@ require('lazy').setup({
     },
   },
 })
--- local nvim_lsp = require('lspconfig')
--- nvim_lsp.denols.setup {
---   on_attach = on_attach,
---   root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
--- }
--- nvim_lsp.tsserver.setup {
---   on_attach = on_attach,
---   root_dir = nvim_lsp.util.root_pattern("package.json"),
---   single_file_support = false
--- }
+local nvim_lsp = require('lspconfig')
+nvim_lsp.denols.setup {
+  on_attach = on_attach,
+  root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
+}
+nvim_lsp.tsserver.setup {
+  on_attach = on_attach,
+  root_dir = nvim_lsp.util.root_pattern("package.json"),
+  single_file_support = false
+}
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
